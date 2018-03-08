ec3bfa0c2edf14b93f3085517d3bd38737e7d5f6
iotoole <iot235@gmail.com>
2018-03-08 14:40:09 -0500

    added alert dialog

--- app/src/main/java/com/iotoole/android/alertsample/MainActivity.java
+++ app/src/main/java/com/iotoole/android/alertsample/MainActivity.java
@@ -1,7 +1,11 @@
 package com.iotoole.android.alertsample;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickShowAlert(View view) {
+        AlertDialog.Builder myAlertBuilder = new AlertDialog.Builder(MainActivity.this);
+
+        // set dialog title
+        myAlertBuilder.setTitle(R.string.alert_title) ;
+
+        // set dialog message
+        myAlertBuilder.setMessage(R.string.alert_message) ;
+
+        // Add the buttons
+        myAlertBuilder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // user clicked OK button
+                Toast.makeText(getApplicationContext(), R.string.pressed_ok,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        myAlertBuilder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // user cancelled the dialog
+                Toast.makeText(getApplicationContext(), R.string.pressed_cancel,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // create and show the AlertDialog
+        myAlertBuilder.show() ;
     }
 }
